b7b16551ca3ed1ff8f9f66136d3af17b97a938c1
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-06-25 18:34:40 +0800

    1

--- src/main/java/com/prostate/assessmen/controller/MedicalExaminationController.java
+++ src/main/java/com/prostate/assessmen/controller/MedicalExaminationController.java
@@ -1,9 +1,5 @@
 package com.prostate.assessmen.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.parser.Feature;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.prostate.assessmen.cache.redis.RedisSerive;
 import com.prostate.assessmen.entity.ProstaticMedicalExamination;
 import com.prostate.assessmen.entity.WechatUser;
@@ -17,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 
 /**
- * 医学检查检验 接口
+ * 医学 检查 检验 接口
  */
 @Slf4j
 @RestController
